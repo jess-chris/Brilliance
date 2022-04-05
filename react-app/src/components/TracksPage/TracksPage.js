79cc1338eaa8bc6ece189519559c014aa94acb51
Jesse Christensen <jtq.christensen@gmail.com>
2022-04-05 15:45:52 -0700

    Fixed empty imports

--- react-app/src/components/TracksPage/TracksPage.js
+++ react-app/src/components/TracksPage/TracksPage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch} from 'react-redux';
-import { Redirect, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import * as trackActions from '../../store/track'; 
 
 const TracksPage = () => {
