eba06bcee5da786daa710628643a2bde4944d6c2
Jesse Christensen <jtq.christensen@gmail.com>
2022-04-06 16:52:46 -0700

    fixes for heroku

--- react-app/src/components/SpecificTrack/SpecificTrack.js
+++ react-app/src/components/SpecificTrack/SpecificTrack.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import * as trackActions from '../../store/track';
 import EditTrackForm from '../EditTrack/EditTrack';
 import AnnoForm from '../AnnoForm/AnnoForm';
