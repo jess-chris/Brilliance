af0502bf0f9a7ae6c390a8a5d2e6c6eb02b372ec
Ty <ty.rickers@gmail.com>
2022-04-08 11:00:46 -0600

    gave upload track same styling as login

--- react-app/src/components/TrackUpload/TrackUpload.js
+++ react-app/src/components/TrackUpload/TrackUpload.js
@@ -37,51 +37,57 @@ const TrackUploadForm = () => {
 
 
     return (
-        <form onSubmit={trackUpload}>
-            <div>
-                <ul className="errors">
-                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-                </ul>
-            </div>
+        <div className='large-container'>
 
-            <div>
-                <label>Track Title</label>
-                <input
-                type='text'
-                name='TrackTitle'
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                ></input>
-            </div>
-            <div>
-                <label>Artist Name</label>
-                <input
-                type='text'
-                name='TrackArtist'
-                value={artist}
-                onChange={(e) => setArtist(e.target.value)}
-                ></input>
-            </div>
-            <div>
-                <label>Lyrics</label>
-                <input
-                type='text'
-                name='TrackLyrics'
-                value={lyrics}
-                onChange={(e) => setLyrics(e.target.value)}
-                ></input>
-            </div>
-            <div>
-                <label>Track Album Image</label>
-                <input
-                type='text'
-                name='TrackImage'
-                value={trackImg}
-                onChange={(e) => setTrackImg(e.target.value)}
-                ></input>
-            </div>
-            <button type='Submit'>Submit</button>
-        </form>
+
+        <div className='input-contatiner'>
+            <form onSubmit={trackUpload}>
+                <div>
+                    <ul className="errors">
+                        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                    </ul>
+                </div>
+
+                <div>
+                    <label>Track Title</label>
+                    <input
+                    type='text'
+                    name='TrackTitle'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    ></input>
+                </div>
+                <div>
+                    <label>Artist Name</label>
+                    <input
+                    type='text'
+                    name='TrackArtist'
+                    value={artist}
+                    onChange={(e) => setArtist(e.target.value)}
+                    ></input>
+                </div>
+                <div>
+                    <label>Lyrics</label>
+                    <input
+                    type='text'
+                    name='TrackLyrics'
+                    value={lyrics}
+                    onChange={(e) => setLyrics(e.target.value)}
+                    ></input>
+                </div>
+                <div>
+                    <label>Track Album Image</label>
+                    <input
+                    type='text'
+                    name='TrackImage'
+                    value={trackImg}
+                    onChange={(e) => setTrackImg(e.target.value)}
+                    ></input>
+                </div>
+                <button type='Submit'>Submit</button>
+            </form>
+        </div>
+        </div>
     )
 }
 
