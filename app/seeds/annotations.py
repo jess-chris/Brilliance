beeacac58a299e311faa20fd11408fcfc16cb7f6
Chris Tsang <christopher.tsang@uconn.edu>
2022-04-11 00:36:49 -0400

    voting button stuff

--- app/seeds/annotations.py
+++ app/seeds/annotations.py
@@ -4,102 +4,103 @@ from datetime import date
 today = date.today()
 
 def seed_annotations():
-    test_anno1 = Annotation(
-        user_id = 1,
-        track_id = 1,
-        content = 'Love this line!',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno2 = Annotation(
-        user_id = 2,
-        track_id = 2,
-        content = 'this is very symbolic',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno3 = Annotation(
-        user_id = 3,
-        track_id = 3,
-        content = 'a metaphor for life',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno4 = Annotation(
-        user_id = 4,
-        track_id = 4,
-        content = 'i felt this',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno5 = Annotation(
-        user_id = 4,
-        track_id = 5,
-        content = 'this part sucks',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno6 = Annotation(
-        user_id = 5,
-        track_id = 6,
-        content = 'this aint it',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno7 = Annotation(
-        user_id = 5,
-        track_id = 6,
-        content = 'mic drop',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno8 = Annotation(
-        user_id = 6,
-        track_id = 7,
-        content = 'im tired',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno9 = Annotation(
-        user_id = 7,
-        track_id = 8,
-        content = '''First to bring a Million Dollar Car to Virginia Beach? Pharrell was one of 
-                    the original owners of the super rare Ferrari Enzo. 
-                    Named after the companies Itialian creator. Only 400 Made.''',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
-    test_anno10 = Annotation(
-        user_id = 8,
-        track_id = 9,
-        content = '''This can be interpreted as him asking someone to reach out. It specifying the palm of the hand implies it being in a helping or caring manner.
-                Later he sings about being on the floor.''',
-        vote_score = 0,
-        created_at = today.strftime("%B %d, %Y"),
-        updated_at = today.strftime("%B %d, %Y")
-    )
+    # test_anno1 = Annotation(
+    #     user_id = 1,
+    #     track_id = 1,
+    #     content = 'Love this line!',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno2 = Annotation(
+    #     user_id = 2,
+    #     track_id = 2,
+    #     content = 'this is very symbolic',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno3 = Annotation(
+    #     user_id = 3,
+    #     track_id = 3,
+    #     content = 'a metaphor for life',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno4 = Annotation(
+    #     user_id = 4,
+    #     track_id = 4,
+    #     content = 'i felt this',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno5 = Annotation(
+    #     user_id = 4,
+    #     track_id = 5,
+    #     content = 'this part sucks',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno6 = Annotation(
+    #     user_id = 5,
+    #     track_id = 6,
+    #     content = 'this aint it',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno7 = Annotation(
+    #     user_id = 5,
+    #     track_id = 6,
+    #     content = 'mic drop',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno8 = Annotation(
+    #     user_id = 6,
+    #     track_id = 7,
+    #     content = 'im tired',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno9 = Annotation(
+    #     user_id = 7,
+    #     track_id = 8,
+    #     content = '''First to bring a Million Dollar Car to Virginia Beach? Pharrell was one of 
+    #                 the original owners of the super rare Ferrari Enzo. 
+    #                 Named after the companies Itialian creator. Only 400 Made.''',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
+    # test_anno10 = Annotation(
+    #     user_id = 8,
+    #     track_id = 9,
+    #     content = '''This can be interpreted as him asking someone to reach out. It specifying the palm of the hand implies it being in a helping or caring manner.
+    #             Later he sings about being on the floor.''',
+    #     vote_score = 0,
+    #     created_at = today.strftime("%B %d, %Y"),
+    #     updated_at = today.strftime("%B %d, %Y")
+    # )
 
-    db.session.add(test_anno1)
-    db.session.add(test_anno2)
-    db.session.add(test_anno3)
-    db.session.add(test_anno4)
-    db.session.add(test_anno5)
-    db.session.add(test_anno6)
-    db.session.add(test_anno7)
-    db.session.add(test_anno8)
-    db.session.add(test_anno9)
-    db.session.add(test_anno10)
+    # db.session.add(test_anno1)
+    # db.session.add(test_anno2)
+    # db.session.add(test_anno3)
+    # db.session.add(test_anno4)
+    # db.session.add(test_anno5)
+    # db.session.add(test_anno6)
+    # db.session.add(test_anno7)
+    # db.session.add(test_anno8)
+    # db.session.add(test_anno9)
+    # db.session.add(test_anno10)
 
-    db.session.commit()
+    # db.session.commit()
+    pass
 
 def undo_annotations():
     db.session.execute('TRUNCATE annotations RESTART IDENTITY CASCADE;')
--- react-app/src/components/Votes/index.js
+++ react-app/src/components/Votes/index.js
@@ -1,21 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import * as trackActions from '../../store/track';
-
-//TODO
-//limit user to one vote per comment or annotation or anno comment
-//if user has already voted on this specific comm/anno,
-//clicking the same button will remove the vote
 
 const voteButtonStyle = {
     backgroundColor: 'white',
     border: 'none',
     color: 'black',
     margin: '10px',
-
+}
+const hideButton = {
+    display: 'none',
+    visibility: 'hidden'
 }
 
+const clickedUp = {
+    backgroundColor: 'white',
+    border: 'none',
+    color: 'green',
+    margin: '10px',
+}
+
+const clickedDown = {
+    backgroundColor: 'white',
+    border: 'none',
+    color: 'red',
+    margin: '10px',
+}
 const Vote = ({ comment_id, anno, annoIdComment, annoCommentId }) => {
     const commentId = comment_id
     const annotationId = anno
@@ -23,7 +34,25 @@ const Vote = ({ comment_id, anno, annoIdComment, annoCommentId }) => {
     const userId = useSelector(state => state.session.user.id)
     const { trackId } = useParams()
 
-   
+    const [style, setStyle] = useState('');
+    const changeStyleDown = async () => {
+        if (style === 'down'){
+            await setStyle('')
+        } else {
+            await setStyle('down');
+        }
+
+    };
+    const changeStyleUp = async () => {
+        if (style === 'up'){
+            await setStyle('')
+        } else {
+            await setStyle('up');
+        }
+
+    };
+
+
     useEffect(() => {
         dispatch(trackActions.getTrackThunk(trackId))
         dispatch(trackActions.getVoteThunk());
@@ -31,18 +60,20 @@ const Vote = ({ comment_id, anno, annoIdComment, annoCommentId }) => {
 
     const handleUpVote = async (e) => {
         e.preventDefault()
+        changeStyleUp()
+        console.log(style)
         //if not a comment AND not a comment on an anno, it is annotation
         if (typeof commentId === 'undefined' && typeof annoCommentId === 'undefined') {
             const commentId = null
             const annotationId = anno
-            
+
             const newVote = {
                 userId,
                 annotationId,
                 commentId,
                 vote: true,
             }
-            
+
             await dispatch(trackActions.createVoteThunk(newVote))
             await dispatch(trackActions.getVoteThunk())
             await dispatch(trackActions.getTrackThunk(trackId))
@@ -61,7 +92,7 @@ const Vote = ({ comment_id, anno, annoIdComment, annoCommentId }) => {
                 commentId,
                 vote: true
             }
-            
+
             await dispatch(trackActions.createVoteThunk(newVote))
             await dispatch(trackActions.getVoteThunk())
             await dispatch(trackActions.getTrackThunk(trackId))
@@ -70,6 +101,7 @@ const Vote = ({ comment_id, anno, annoIdComment, annoCommentId }) => {
         //therefore it is a track comment
         else if (typeof annotationId === 'undefined') {
             const annotationId = null
+
 
             const newVote = {
                 userId,
@@ -82,11 +114,13 @@ const Vote = ({ comment_id, anno, annoIdComment, annoCommentId }) => {
             await dispatch(trackActions.getVoteThunk())
             await dispatch(trackActions.getTrackThunk(trackId))
         }
-        
+
     }
     //downvote is same as upvote but vote:false 
     const handleDownVote = async (e) => {
         e.preventDefault()
+        changeStyleDown()
+        console.log(style)
 
         if (typeof commentId === 'undefined' && typeof annoCommentId === 'undefined') {
             const commentId = null
@@ -131,18 +165,41 @@ const Vote = ({ comment_id, anno, annoIdComment, annoCommentId }) => {
             await dispatch(trackActions.getVoteThunk())
             await dispatch(trackActions.getTrackThunk(trackId))
         }
-        
+
     }
 
     return (
         <>
-        
-        <div id='thumbs-up'>  
-            <button style={voteButtonStyle} onClick={handleUpVote}><i className="fa-regular fa-thumbs-up"></i></button>
-            </div>
-            <div>
-            <button style={voteButtonStyle} onClick={handleDownVote}><i className="fa-regular fa-thumbs-down"></i></button>
-        </div>
+            {style === '' &&
+                <>
+                    <div id='thumbs-up'>
+                        <button style={voteButtonStyle} onClick={handleUpVote}><i className="fa-regular fa-thumbs-up"></i></button>
+                    </div>
+                    <div>
+                        <button style={voteButtonStyle} onClick={handleDownVote}><i className="fa-regular fa-thumbs-down"></i></button>
+                    </div>
+                </>
+            }
+            {style === 'up' &&
+                <>
+                    <div id='thumbs-up'>
+                        <button style={clickedUp} onClick={handleUpVote}><i className="fa-regular fa-thumbs-up"></i></button>
+                    </div>
+                    <div>
+                        <button style={hideButton} onClick={handleDownVote}><i className="fa-regular fa-thumbs-down"></i></button>
+                    </div>
+                </>
+            }
+            {style === 'down' &&
+                <>  
+                    <div id='thumbs-up'>
+                        <button style={hideButton} onClick={handleUpVote}><i className="fa-regular fa-thumbs-up"></i></button>
+                    </div>
+                    <div>
+                        <button style={clickedDown} onClick={handleDownVote}><i className="fa-regular fa-thumbs-down"></i></button>
+                    </div>
+                </>
+            }
         </>
     )
 }
